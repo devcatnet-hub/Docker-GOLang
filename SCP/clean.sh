0d773b6030cd6634cd62c66b7972972f41760c06
deathwalker66 <storage.catnet@gmail.com>
2026-04-20 21:50:07 -0600

    [20/04/2026] | [21:50] - First Commit

---
+++ SCP/clean.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+### Universal
+    # Paquetes temporales
+    rm -rf /var/cache/apk/* /var/cache/apt/* /var/lib/apt/lists/* /tmp/* /var/tmp/*
+    # Cache de paquetes
+    rm -rf ~/.cache /root/.cache
+
+### Specific
+    rm -rf /usr/local/go/pkg/bootstrap
+    rm -rf /usr/local/go/pkg/obj
+    rm -rf /usr/local/go/pkg/*/cmd
+    rm -rf /usr/local/go/pkg/tool/*/api
+    rm -rf /usr/local/go/pkg/tool/*/go_bootstrap
+    rm -rf /usr/local/go/src/cmd/dist/dist
+
+    go clean -modcache && \
+    rm -rf $(go env GOCACHE) && \
+    rm -rf /go/pkg/mod/cache/*
+
+    rm -rf \
+    /usr/share/doc/* \
+    /usr/share/man/* \
+    /usr/share/info/*
+
+    
---
+++ SCP/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+### Add /SCP Dir to PATH
+export PATH=$PATH:/SCP:/COM
+
+### Add +x Scripts in /COM and /SCP Dir
+chmod +x /COM/*
+chmod +x /SCP/*
+
+### Simplication
+if [ "$SIMPLIFICATED" == "NO" ]; then
+                    echo "Run All Services...Done!"
+                    export PATH="/COM:$PATH"
+                    nonsimplificated.sh
+elif [ "$SIMPLIFICATED" == "YES" ]; then
+                    echo "Simplicated...Done!"                    
+                    simplificated.sh
+fi
